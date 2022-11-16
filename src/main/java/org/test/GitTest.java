552a37baac8d8d1444dc688961791e39cbcf8621
fickler <2586844575@qq.com>
2022-11-16 19:38:20 +0800

    first commit

--- src/main/java/org/test/GitTest.java
+++ src/main/java/org/test/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("git test 。。。");
         System.out.println("git test !!!!");
         System.out.println("git test ?????");
+        System.out.println("git test ,,,,");
     }
 }
