429e3db4408bf1e331f6123de06e43351d5b4412
fickler <2586844575@qq.com>
2022-11-16 11:48:13 +0800

    first commit

---
+++ src/main/java/org/test/GitTest.java
@@ -0,0 +1,11 @@
+package org.test;
+
+/**
+ * @author dell
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test 。。。");
+    }
+}
